9f46772a9927f098debe493fa2d52f5abf825b22
Дима Дубилей <dimon.pr.2007@gmail.com>
2024-09-06 15:26:23 +0300

    Я добавил что-то

--- 16.09/16.09.cpp
+++ 16.09/16.09.cpp
@@ -4,4 +4,12 @@ using namespace std;
 
 int main()
 {
+	int naml;
+	cout << "I like money!!!!! \n";
+	cout << "Enter nam: ";
+
+	cin >> naml;
+
+	cout << "NO!";
+	cout << "I's" << naml * 2 << "!";
 }
